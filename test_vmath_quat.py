8ba80d76abd786349454bdf9769b8f175b35e53f
Janet Rossini <valkyriejanet@gmail.com>
2024-02-09 15:49:25 -0500

    added slerp seems to work, tested lightly.

--- test_vmath_quat.py
+++ test_vmath_quat.py
@@ -20,3 +20,21 @@ class TestVMathQuaternion:
         assert v2.x == pytest.approx(3, abs=0.001)
         assert v2.y == pytest.approx(2, abs=0.001)
         assert v2.z == pytest.approx(-1, abs=0.001)
+
+    def test_slerp(self):
+        axis = Vector((1, 2, 3))
+        q1 = Quaternion(axis, 0)
+        q2 = Quaternion(axis, radians(90))
+        q3 = q1.slerp(q2, 0.5)
+        # Quaternion(0.924, 0.102, 0.205, 0.307) != 0
+        # value borrowed from test of mathutils slerp.
+        assert q3.w == pytest.approx(0.924, abs=0.001)
+        assert q3.x == pytest.approx(0.102, abs=0.001)
+        assert q3.y == pytest.approx(0.205, abs=0.001)
+        assert q3.z == pytest.approx(0.307, abs=0.001)
+
+    def test_error(self):
+        with pytest.raises(TypeError) as info:
+            q1 = Quaternion(1, 2, 3, 4, "hello")
+        assert "Object cannot be initialized from (1, 2, 3, 4, 'hello')" in str(info.value)
+
