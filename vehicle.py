61edb2d3e6210e5bc9f8520957dc878d52f4ba78
Janet Rossini <valkyriejanet@gmail.com>
2024-02-09 15:49:25 -0500

    extract quaternion-making methods

--- vehicle.py
+++ vehicle.py
@@ -90,24 +90,32 @@ class Vehicle:
             return angle_deg
 
     def _new_vehicle_without_yaw(self):
+        remove_yaw = self._yaw_quaternion()
+        new_forward = remove_yaw@self.forward
+        new_upward = remove_yaw@self.upward
+        return Vehicle(Vector((0, 0, 0)), new_upward, new_forward)
+
+    def _yaw_quaternion(self):
         z_axis = Vector((0, 0, 1))
         rise = self.forward.y
         run = self.forward.x
         angle = atan2(rise, run)
         remove_yaw = Quaternion(z_axis, -angle)
-        new_forward = remove_yaw@self.forward
-        new_upward = remove_yaw@self.upward
-        return Vehicle(Vector((0, 0, 0)), new_upward, new_forward)
+        return remove_yaw
 
     def _new_vehicle_without_pitch(self):
+        remove_pitch = self._pitch_quaternion()
+        new_forward = remove_pitch@self.forward
+        new_upward = remove_pitch@self.upward
+        return Vehicle(Vector((0, 0, 0)), new_upward, new_forward)
+
+    def _pitch_quaternion(self):
         y_axis = Vector((0, 1, 0))
         rise = self.forward.z
         run = self.forward.x
         angle = atan2(rise, run)
         remove_pitch = Quaternion(y_axis, angle)  # why not -?
-        new_forward = remove_pitch@self.forward
-        new_upward = remove_pitch@self.upward
-        return Vehicle(Vector((0, 0, 0)), new_upward, new_forward)
+        return remove_pitch
 
     def _new_vehicle_with_roll_only(self):
         return self._new_vehicle_without_yaw()._new_vehicle_without_pitch()
