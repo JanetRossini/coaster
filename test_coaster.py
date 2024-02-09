cd7213f766cede2f1ac01a7ed1f770a4bd28b86a
Janet Rossini <valkyriejanet@gmail.com>
2024-02-09 15:49:25 -0500

    commentary.

--- test_coaster.py
+++ test_coaster.py
@@ -3,14 +3,18 @@ from math import atan2
 import pytest
 from mathutils import Vector, Quaternion
 
+from test_data import tilt_45, fetch
+from vehicle import Vehicle
 
-class TestCoaster:
+
+class TestCoasterVehicle:
 
     def test_hookup(self):
         assert 2+2 == 4
 
     def test_remove_yaw(self):
         # yaw is rotation around z, uses y and x
+        # we want y == 0
         forward = Vector((1, 1, 1)).normalized()
         # had to install mathutils for Vector to be recognized
         # top view is x horizontal, y vertical
@@ -24,16 +28,54 @@ class TestCoaster:
 
     def test_remove_pitch(self):
         # pitch is rotation around y, uses z and x
-        forward = Vector((1, 1, 1)).normalized()
+        # we want z == 0
+        forward = Vector((1, 2, 3)).normalized()
         # side view is y horizontal, z vertical
         y_axis = Vector((0, 1, 0))
-        rise = forward.x
-        run = forward.z
+        rise = forward.z
+        run = forward.x
         angle = atan2(rise, run)
-        remove_pitch = Quaternion(y_axis, -angle)
+        remove_pitch = Quaternion(y_axis, angle)  # why not -?
         f_no_pitch = remove_pitch@forward
         assert f_no_pitch.z == pytest.approx(0, abs=0.001)
 
-    def test_vehicle(self):
-        pass
+    def test_vehicle_yaw(self):
+        back, up, front = fetch(tilt_45, 0)
+        vehicle = Vehicle(back, up, front)
+        yaw_removed = vehicle._new_vehicle_without_yaw()
+        assert yaw_removed.forward.y == pytest.approx(0, abs=0.001)
+
+    def test_vehicle_pitch(self):
+        back, up, front = fetch(tilt_45, 0)
+        vehicle = Vehicle(back, up, front)
+        pitch_removed = vehicle._new_vehicle_without_pitch()
+
+    def test_vehicle_with_roll_only(self):
+        back, up, front = fetch(tilt_45, 0)
+        vehicle = Vehicle(back, up, front)
+        roll_only = vehicle._new_vehicle_with_roll_only()
+        assert roll_only.forward.y == pytest.approx(0, abs=0.001)
+        assert roll_only.forward.z == pytest.approx(0, abs=0.001)
+
+    def test_roll(self):
+        back, up, front = fetch(tilt_45, 0)
+        vehicle = Vehicle(back, up, front)
+        roll_degrees = vehicle.roll_degrees()
+        assert roll_degrees == pytest.approx(16.1, abs=0.1)
+
+    def test_all_roll(self):
+        prior = 0
+        print()
+        for i in range(0,9):
+            vehicle = Vehicle(*fetch(tilt_45, i))
+            roll_degrees = vehicle.roll_degrees()
+            print(i, roll_degrees, roll_degrees - prior)
+            if prior:
+                diff = roll_degrees - prior
+                assert diff == pytest.approx(-45, abs=0.1) or diff == pytest.approx(315, abs=0.1)
+            prior = roll_degrees
+
+
+
+
 
