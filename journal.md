b50bcb3ba31065375f853c4bfa686c03d79c27a7
Janet Rossini <valkyriejanet@gmail.com>
2024-04-06 08:57:36 -0400

    Reset to Thursday, move VtFileWriter out. Tests are green.

--- journal.md
+++ journal.md
@@ -54,3 +54,108 @@ needs pulling. I do not think that PyCharm will warn us if someone
 else has pushed. We'll find out. Hopefully with good news.
 
 dizzi woz 'ere
+
+## 20240405 (JR)
+
+This morning I'm going to experiment with moving things around. In 
+particular, I'd like to get down to just one Vehicle class, in its 
+own file, and to get VTFileWriter moved out of the test files.
+
+I plan to try these things and then roll back. Will report what 
+happens here.
+
+I start  with Git / Pull. All files are up to date, as I expected.
+
+First experiment, cut Vehicle out of rollercoastergen and past 
+over the existing one in vehicle.py.
+
+### Bump
+
+Before I even get there, I discover that both VtFileWriter and 
+Vehicle are combined into rollercoastergen. This will not do. Why 
+not? Because when changes are needed, they'll need to be done in 
+multiple places.
+
+I will try deleting mine, on the grounds that Dizzi's are newer. 
+First, though, I'll diff them.
+
+Ah. Some of your copies have CRLF line ends, not just LF. I think 
+we should  change your prefs on that, tho it may be mostly harmless.
+
+I've just spent mondo time fiddling with the LF / CRLF thing.
+
+OK, moved VtFileWriter out of the tests into its own file. Will 
+commit that.
+
+We'll leave you with your own for now, try removing them when 
+we're together.
+
+Ran into trouble with Vehicle and its testing, because I use my 
+own Vector and Quaternion classes, and you use blender mathutils, 
+and, unfortunately for us all, mathutils in Blender has features 
+that we seem not to have in Python's regular mathutils.
+
+Best I can figure out right now, that means we can't have both a 
+testable Vehicle class and one that is usable in Blender, unless 
+we use my Q and V classes. Which we could do and should try, I guess.
+
+
+### 0925
+
+I'm gonna take a break. Eye bothering me this morning for some reason.
+
+## 20240406 (JR Saturday)
+
+By moving enough files to `modules` I was able to get Blender to 
+act like it has accepted the file. I had to change the import in 
+rollercoastergen from `from modules/vtfilewriter` to `from 
+vtfilewriter`, which will of course not work in the tests. I did 
+not know how to test the script, but at least it loaded.
+
+I'm going to reset back to end of day Thursday and work from there.
+
+Done. 
+
+Now to figure out what to do.
+
+I am quite sure that there are things we do not know about Blender 
+add-ons, and things about Python modules, that are getting in the 
+way here.
+
+Some steps that could/should be taken include:
+
+1. Make the two copies of Vehicle and VtFileWriter the same. In 
+   the case of VtFileWriter, I think it needs improvement but 
+   maybe that can wait. I think these absolutely must be done.
+2. Try using `from . import thing`, which I saw somewhere as 
+   something you can do with Blender.
+3. Get the fake bpy files, which we may be able to use to help the 
+   refactoring go more smoothly, or at least get rid of warnings, 
+   and maybe even get prompting in PyCharm.
+
+Found this idea, which would allow us to use different folders in 
+blender from those in our local code. Seems nasty but might work.
+
+~~~python
+if __name__ == '__main__':
+    import sys
+    import os
+    rootdir = os.path.dirname(os.path.realpath(__file__))
+    if rootdir not in sys.path:
+        sys.path.append(rootdir)
+    try:
+        # Pycharm import
+        from support import *
+    except ModuleNotFoundError:
+        # Blender Text Editor import
+        from addon_folder.support import *
+~~~
+
+I have posted a question on Blender StackExchange, hoping for good 
+ideas.
+
+I've moved VtFileWriter out of rollercoastergen. May have to put it 
+back, but I think the try/except horrid though it is, can be made 
+to work.
+
+0850, gonna take a break.
--- test_coaster.py
+++ test_coaster.py
@@ -8,6 +8,7 @@ from v_vector import Vector
 from v_quaternion import Quaternion
 from test_data import tilt_45, fetch
 from vehicle import Vehicle
+from vtfilewriter import VtFileWriter
 
 
 class TestCoasterVehicle:
@@ -272,7 +273,7 @@ get_data() {
         filepath = os.path.expanduser("~/Desktop")
         size = 4
         writer = VtFileWriter(verts, filepath, "test", size)
-        writer.write_files()
+        writer.write_files("ignored", False, True)
         # writer.make_output()  # uncomment to look at things
         # assert False
 
@@ -284,99 +285,3 @@ class Co:
     def __repr__(self):
         v = self.co
         return f"<{v.x}, {v.y}, {v.z}>"
-
-
-# VtFileWriter begins here
-class VtFileWriter:
-    def __init__(self, vertices, path, base_name, size):
-        self.vertices = vertices
-        self.path = path
-        self.base_name = base_name
-        self.size = size
-
-    def write_files(self):
-        coords = tuple(v.co for v in self.vertices)
-        triples = tuple(coords[i:i + 3] for i in range(0, len(coords) - 1, 2))
-        all_lines = self.make_lines(triples)
-        count = ceil(len(triples)/self.size)
-        for file_number in range(count):
-            start = file_number*self.size
-            end = (file_number+1)*self.size
-            lines = all_lines[start:end]
-            name = [self.base_name, str(file_number)]
-            file_name = "_".join(name) + ".lsl"
-            full_path = os.path.join(self.path, file_name)
-            with open(full_path, "w") as file:
-                self.write_one_file(file_name, file_number, count, lines, file)
-                print(f"File was written to {full_path}\n")
-
-    def write_one_file(self, file_name, file_number, file_count, lines, file):
-        from datetime import datetime
-        now = datetime.now()
-        file.write(f"// {file_name}\n")
-        time = now.strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"// {time}\n")
-        file.write("//    created by VtFileWriter\n")
-        file.write("//    JR 20240115 - inert, last sends LOADING_DONE\n")
-        file.write("//    Script names do not matter.\n\n")
-        file.write(f"integer SCRIPT_NUMBER = {file_number};\n")
-        file.write(f"integer LAST_SCRIPT_NUMBER= {file_count-1};\n")
-        file.write(f"integer CHUNK_SIZE = {self.size};\n\n")
-        file.write("list data = [\n")
-        text = "\n,".join(lines)
-        file.write(text)
-        file.write("\n];\n")
-        file.write(self.fixed_part)
-
-    @staticmethod
-    def make_lines(coordinate_triples):
-        lines = []
-        back_zero = coordinate_triples[0][0]
-        for back, up, front in coordinate_triples:
-            back_zeroed = back - back_zero
-            roll = Vehicle(back, up, front).roll_degrees()
-            output = f"<{back_zeroed.x:.3f}, {back_zeroed.y:.3f}, {back_zeroed.z:.3f}, {roll:.0f}>"
-            lines.append(output)
-        return lines
-
-    fixed_part = """
-// nothing varies from here on down
-
-write_data() {
-    integer limit = llGetListLength(data);
-    integer out_key = CHUNK_SIZE*SCRIPT_NUMBER;
-    integer end_key = out_key + limit;
-    // llSay(0, llGetScriptName() + " writing " + (string) out_key + " up to " + (string) end_key);
-    integer index;
-    for (index = 0; index < limit; index++, out_key++) {
-        llLinksetDataWrite("datakey"+(string) out_key,  llList2String( data , index));
-    }
-    if (SCRIPT_NUMBER == LAST_SCRIPT_NUMBER) {
-        integer keyCount = llLinksetDataCountKeys(); 
-        // llSay(0, "SIGNALLING LOADING_DONE " + (string) keyCount);
-        llMessageLinked(LINK_THIS, keyCount, "LOADING_DONE", NULL_KEY);
-    } else {
-        llMessageLinked(LINK_THIS, SCRIPT_NUMBER + 1, "LOADING", NULL_KEY);
-    }
-}
-
-default {
-    on_rez(integer start_param) {
-        llResetScript();
-    }
-
-    state_entry() {
-    }
-
-    link_message(integer sender_num, integer num, string str, key id) {
-        if (str != "LOADING") return;
-        if (num != SCRIPT_NUMBER) return;
-        if (SCRIPT_NUMBER == 0) {
-            llLinksetDataReset();
-            // llSay(0, "SCRIPT 0 Resetting LSD");
-        }
-        write_data();
-    }
-}
-"""
-# VtFileWriter ends here
